9353505e075cdba5eafd4da79c24c8eb36e13d37
r-leyshon <richard.leyshon@ons.gov.uk>
2023-10-14 06:31:36 +0100

    feat: Table filter works with in to find mutliple repo issues / PRs

--- app.py
+++ app.py
@@ -10,16 +10,18 @@ app_ui = ui.page_fluid(
     ui.input_selectize(
         id="repo_filter",
         label="Select a Repo:",
+        multiple=True,
         choices=reps,
     ),
     ui.input_radio_buttons(
         id="type_filter",
         label="Select Type:",
-        choices=["issue", "pr"],
-        selected="issue",
+        choices=["all", "issues", "pull requests"],
+        selected="all",
         inline=True,
     ),
     ui.output_data_frame("table"),
+    # ui.output_text("table")
 )
 
 
@@ -28,12 +30,30 @@ def server(input, output, session):
 
     @output
     @render.data_frame
+    # @render.text
     def table():
         """Return the optionally filtered table object."""
         r = input.repo_filter()
         t = input.type_filter()
-        q_string = f"name == '{r}' and type == '{t}'"
+        if t == "issues":
+            type_sel = "issue"
+        elif t == "pull requests":
+            type_sel = "pr"
+
+        if len(r) == 0 and t == "all":
+            # no repo selected and 'all' type is selected
+            return dat
+        elif len(r) == 0:
+            # no repo selected, 'all' type is not selected
+            q_string = f"type == '{type_sel}'"
+        elif t == "all":
+            # min 1 repo selected and 'all' type is not selected
+            q_string = f"name in {r}"
+        else:
+            # min 1 repo selected & 'all' type is not selected
+            q_string = f"name in {r} and type == '{type_sel}'"
         return dat.query(q_string)
+        # return q_string
 
 
 app = App(app_ui, server)
